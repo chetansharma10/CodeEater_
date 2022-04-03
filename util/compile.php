911e07a325134277c32dd38a7231f1c732c73b76
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2022-04-04 01:01:11 +0530

    added More functionality

--- util/compile.php
+++ util/compile.php
@@ -4,9 +4,11 @@
     {  
       $ret=null;
       $c_time=null;
-      exec($compile, $c_time, $ret);
+      exec($compile." 2>&1", $c_time, $ret); 
+      //By this way we are fetching error or time
       
-
+      //if there is some error in code then $ret ==  become 0 which means this cmd have
+      // Some Error So we will not move forward
       if($ret==0)
       {
           $res=array(
@@ -14,7 +16,7 @@
             'output' => $c_time,
             'succ' => 0
           );
-          print_r($res);
+          echo json_encode($res);
       }
       else
       {
@@ -26,10 +28,10 @@
             $res=array(
                   'message'=> "Successfully Compiled 🎉",
                   'ctime'=>$c_time,
-                  'ret' => $ret,
-                  'output'=>$output
+                  'output'=>$output,
+                  'succ' => 1
             );
-            print_r($res);
+            echo json_encode($res);
 
           }
           else
@@ -39,7 +41,7 @@
               'output' => $output,
               'succ' => 0
             );
-            print($res);
+            echo json_encode($res);
           }
         
       }
@@ -47,13 +49,16 @@
 
     if($_POST)
     {
-
-        $time=new DateTime();
+      
+        $time=new DateTime(); //First We will fetch the current at which time client come
+        
+        //then create the code File
         $codefilename=$time->format('d-m-Y-H-i-s').".".$_POST["ext"];
         $file=fopen("codeFiles/".$codefilename,"w") or die("Unable to Open the File");
         fwrite($file,$_POST["code"]);
         fclose($file);
 
+        //then Create the input file
         $infilename=$time->format('d-m-Y-H-i-s');
         $file=fopen("codeFiles/".$infilename,"w") or die("Unable to Open the File");
         fwrite($file,$_POST['in']);
@@ -74,9 +79,40 @@
             break;
 
           case 'py':
-            $compile='time g++ -o ./codeFiles/'.str_replace('.c','.exe',$filename).'./codeFiles/'.$filename;
-            $execute='./codeFiles/'.$filename;
-            runCmd($compile,$execute);
+            $execute='/usr/bin/time -o ./codeFiles/'.$infilename.'_time cat ./codeFiles/'.$infilename.' | python3 ./codeFiles/'.$codefilename.' 2> ./codeFiles/'.$infilename.'_err';
+            $c_time='cat ./codeFiles/'.$infilename.'_time';
+            $err='cat ./codeFiles/'.$infilename.'_err';
+
+            $output=null;
+            $ret=null;
+            $tim=null;
+            $errr=null; 
+
+            exec($execute,$output,$ret);
+            exec($c_time,$tim);
+            
+          if($ret==0)
+          {
+            $res=array(
+                  'message'=> "Successfully Compiled",
+                  'ctime' => $c_time,
+                  'output'=>$output,
+                  'succ'=> 1
+            );
+            echo json_encode($res);
+          }
+          else
+          {
+            exec($err,$errr);
+            $res=array(
+              'message' => "Some Error Caused 🤦",
+              'output' => $errr,
+              'succ' => 0
+            );
+
+            echo json_encode($res);
+            }
+
             break;
 
           default:
@@ -85,11 +121,14 @@
               'output' => "This Language Not Supported by the System!",
               'succ' => 0
             );
-            print($res);
+           echo json_encode($res);
        
        }
         
     }
+
+
+    //For Random Request
     else
     {
         echo "Bad Request!";
